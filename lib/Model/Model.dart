284ae06a339e153aac2aeba93a70bf5146b9248b
Domenico Posillipio <domposi@gmail.com>
2020-06-15 18:23:07 +0200

    creati model per Utente

--- lib/Model/Model.dart
+++ lib/Model/Model.dart
@@ -14,4 +14,70 @@ class Img {
     );
   }
 
-}
+}//Img
+
+class Utente {
+  var id, name, username, email, phone, website;
+  Address address;
+  Company company;
+
+  Utente({this.id, this.name, this.username, this.email, this.phone,
+      this.website, this.address, this.company});
+
+  factory Utente.fromJson(Map<String,dynamic> json){
+    return Utente(
+      id:  json['id'],
+      name: json['name'],
+      username: json['username'],
+      email: json['email'],
+      phone: json['phone'],
+      website: json['website'],
+      address: new Address(),
+      company: new Company(),
+    );
+  }
+}//Utente
+
+class Address {
+  var street, suite, city, zipcode;
+  Geo geo;
+
+  Address({this.street, this.suite, this.city, this.zipcode, this.geo});
+
+  factory Address.fromJson(Map<String,dynamic> json){
+    return Address(
+      street: json['street'],
+      suite: json['suite'],
+      city: json['city'],
+      zipcode: json['zipcode'],
+      geo: new Geo(),
+    );
+  }
+}//Address
+
+class Geo {
+  var lat, lng;
+
+  Geo({this.lat, this.lng});
+
+  factory Geo.fromJson(Map<String,dynamic> json){
+    return Geo(
+      lat: json['lat'],
+      lng: json['lng'],
+    );
+  }
+}//Geo
+
+class Company {
+  var name, catchPhrase, bs;
+
+  Company({this.name, this.catchPhrase, this.bs});
+
+  factory Company.fromJson(Map<String,dynamic> json){
+    return Company(
+      name: json['name'],
+      catchPhrase: json['catchPhrase'],
+      bs: json['bs'],
+    );
+  }
+}//Company
