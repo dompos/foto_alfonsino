aff8f9d830c4b5f23a4f63849c103f9a57aad21e
Domenico Posillipio <domposi@gmail.com>
2020-06-15 18:50:02 +0200

    creato widget per la lista degli utenti

--- lib/Users.dart
+++ lib/Users.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fotoalfonsino/Model/Model.dart';
+import 'package:fotoalfonsino/Service/Service.dart';
 
 class Users extends StatefulWidget{
   Users({Key key}) : super(key: key);
@@ -19,7 +21,39 @@ class _UsersState extends State<Users>{
         ),
         backgroundColor: Color(0xff232426),
       ),
-      body: Container(),
+      body: FutureBuilder<List<Utente>>(
+        future: Service.fetchUtente(),
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            return ListView.separated(
+              padding: const EdgeInsets.all(8),
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 50,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        '${snapshot.data[index].username}',
+                        style: TextStyle(color: Colors.white70),
+                      ),
+                      Text(
+                        '${snapshot.data[index].email}',
+                        style: TextStyle(color: Colors.white70),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) => const Divider(),
+            );
+          }else if(snapshot.hasError){
+            return Text('ERRORE');
+          }
+          return Center( child: CircularProgressIndicator());
+        },
+      ),
       backgroundColor: Color(0xff18191a),
     );
   }
